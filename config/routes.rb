568422e97a81648f4d71ebb00d4c783201d2c605
Elizabeth Parisi <parisielizabeth@gmail.com>
2020-03-14 23:06:10 -0400

    added static controller + home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+
+  root to: "static#home"
+
+
   resources :comments
   resources :recommendations
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
-
-
 end
