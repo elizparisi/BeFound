3bc0543c116a75796049cb62e516170af8cc2d70
Elizabeth Parisi <parisielizabeth@gmail.com>
2020-03-16 19:14:01 -0400

    add has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :recommendations
   has_many :comments
   has_many :commented_recommendations, through: :comments, source: :recommendation
+
+  has_secure_password
 end
