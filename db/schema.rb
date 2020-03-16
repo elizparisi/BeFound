2dea677bd3a4943e8c9e599b494b6dd2a4f744c8
Elizabeth Parisi <parisielizabeth@gmail.com>
2020-03-16 19:02:04 -0400

    revised schema + fixed typo in user model

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_03_14_201918) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recommendations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,4 +50,5 @@ ActiveRecord::Schema.define(version: 2020_03_14_201918) do
 
   add_foreign_key "comments", "recommendations"
   add_foreign_key "comments", "users"
+  add_foreign_key "recommendations", "users"
 end
