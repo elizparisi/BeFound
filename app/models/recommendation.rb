a03f4a6b1c44aa64a3c2934bd39eb470002769f8
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-03-23 16:38:09 -0400

    add validations to recommendation model

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,5 +1,7 @@
 class Recommendation < ApplicationRecord
 
+  validates :image_url, :name, :loction, :description, presence: true
+
   belongs_to :user
   has_many :comments
   has_many :users, through: :comments
